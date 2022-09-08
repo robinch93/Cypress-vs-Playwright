719b22c533a7d4e9e1c8c3ece6ea7a0d49bcdc1b
robin <34096241+robinch93@users.noreply.github.com>
2022-09-08 22:18:38 +0200

    update navigation endpoint

--- api/rest/endpoints/navigations.js
+++ api/rest/endpoints/navigations.js
@@ -11,8 +11,7 @@ const getNavigations = async () => {
 
     $('ul.top-menu li:not(ul.sublist li)').each(function (i, element) {
         let menuLink = $(element).find('a').attr('href')
-        let menuName = $(element).find('a').text()
-        menuName = getText(menuName)
+        let menuName = $(element).find('a:first').text().trim()
         topmenu.push(menuName)
         links[menuName] = baseUrl + menuLink
     })
@@ -25,13 +24,42 @@ const getNavigations = async () => {
     return data
 }
 
+const getMenuNav = async (menuName) => {
+    const $ = await getData()
+    let data = {}
+
+    let submenu = []
+    let links = {}
+    let baseElement = $(`ul.top-menu li:contains("${menuName}")`).first().html()
+    $(baseElement).find('ul.sublist li').each(function (i, element) {
+        let menuLink = $(element).find('a').attr('href')
+        let menuName = $(element).find('a').text().trim()
+        submenu.push(menuName)
+        links[menuName] = baseUrl + menuLink
+    })
+
+    data = {
+        submenu,
+        links
+    }
+
+    return data
+}
+
 const navigations = async (req, res) => {
     const data = await getNavigations()
     res.status(200).send(data)
 }
 
+const menuNavigations = async (req, res) => {
+    let name = req.params.menuName
+    const data = await getMenuNav(name)
+    res.status(200).send(data)
+}
+
 const navRoutes = (app) => {
     router.get('/topmenu', navigations)
+    router.get('/menu/:menuName', menuNavigations)
     app.use(router)
 }
 
